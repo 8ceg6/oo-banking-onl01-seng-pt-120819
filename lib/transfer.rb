e5674dcd7f0acf37649dfa8bdd38a69ef3bb36f8
cedric e goddard <8ceg6@comcast.net>
2020-01-21 21:31:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -41,7 +41,7 @@ class Transfer
   def reverse_transfer
     if status == "complete"
        sender.deposit(amount)
-       receiver.deposit(amount  -1)
+       receiver.deposit(amount * -1)
       @status = "reversed"
     end 
   end 
