299332528c6889ff394cfb65e8d1646707a3871c
cedric e goddard <8ceg6@comcast.net>
2020-01-21 21:06:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :sender, :receiver, :status
+  
+  def initialize(sender, receiver, amount) 
+      @amount = amount
+      @sender = sender 
+      @receiver = receiver 
+      @status = 'pending'
+  end 
+  
+  
+  # def status
+  #     @status
+  # end 
+  
+  def amount 
+      @amount 
+  end 
+  
+  def valid?
+      if sender.valid? && receiver.valid? 
+        true 
+      else 
+        false
+      end 
+  end 
+  
+  def execute_transaction
+      
+  end 
+ 
 end
